aab3195e5b23f04117e749e4ac38b3f027626f0b
Krystof Gartner <krystofgartner@proton.me>
2025-03-13 23:56:06 +0100

    Split up files

---
+++ encoder.c
@@ -0,0 +1,17 @@
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_WH_D);
+        } else {
+            tap_code(KC_WH_U);
+        }
+    }
+    return true;
+}
